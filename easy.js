53dcfa4097e6cecf64fe9b43d59bc3082081362b
Ashley Richard <ashleymrichard@gmail.com>
2022-10-08 20:24:47 -0700

    adds Return a String as an Integer

--- easy.js
+++ easy.js
@@ -1,8 +1,16 @@
 // Add up the Numbers from a Single Number
 
-function addUp(num) {
-    var res = (num * (num + 1)) / 2;
-    return res
+// function addUp(num) {
+//     var res = (num * (num + 1)) / 2;
+//     return res
+// }
+
+// console.log(addUp(600))
+
+// Return a String as an Integer
+
+function stringInt(str) {
+	return parseInt(str)
 }
 
-console.log(addUp(600))
+console.log(stringInt("6"))
