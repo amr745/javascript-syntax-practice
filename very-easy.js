56d25f041f567993cd3594189bedc223474d3dfd
Ashley Richard <ashleymrichard@gmail.com>
2022-10-08 21:17:57 -0700

    adds How Many D's Are There?

--- very-easy.js
+++ very-easy.js
@@ -250,5 +250,18 @@
 
 // console.log(comp("hello", "edabit"))
 
+// How Many D's Are There?
 
+function countDs(sentence) {
+	// make sentenct lower case
+    // match the d's
+    // count the d's
 
+    // let lower = sentence.toLowerCase()
+    // let letter = lower.match(/d/g)
+    // return letter.length
+
+    return sentence.match(/d/g).length
+}
+
+console.log(countDs("The rodents hibernated in their den."))
