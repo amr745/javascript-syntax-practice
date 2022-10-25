92b4fcf27b03cbb8ad209cfd77677f5023343a53
Ashley Richard <ashleymrichard@gmail.com>
2022-10-24 22:06:40 -0700

    adds Century Crisis

--- 10-22-22.js
+++ 10-22-22.js
@@ -1,7 +1,15 @@
 //1. I'd Like a New Shade of Blue, Please
 
-function howManyWalls(n, w, h) {
-    return Math.floor(n / (w * h))
+// function howManyWalls(n, w, h) {
+//     return Math.floor(n / (w * h))
+// }
+
+// console.log(howManyWalls(10, 15, 12))
+
+//2. Century Crisis
+
+function futurePeople(population, n) {
+	return population + 360 * n
 }
 
-console.log(howManyWalls(10, 15, 12))
+console.log(futurePeople(5240, 3))
