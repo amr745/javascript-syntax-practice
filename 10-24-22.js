557d8ab4dca82538867a466e584127d9598234e7
Ashley Richard <ashleymrichard@gmail.com>
2022-10-26 03:02:35 -0700

    adds Stack the Boxes

--- 10-24-22.js
+++ 10-24-22.js
@@ -1,10 +1,18 @@
 //1. Fix the Bug: Simple Array Manipulation
 
-function incrementItems(arr) {
-	for (let i = 0; i < arr.length; i++) {
-        arr[i]++
-    }
-    return arr
+// function incrementItems(arr) {
+// 	for (let i = 0; i < arr.length; i++) {
+//         arr[i]++
+//     }
+//     return arr
+// }
+
+// console.log(incrementItems([-1, -2, -3, -4]))
+
+//2. Stack the Boxes
+
+function stackBoxes(n) {
+	return Math.pow(n, 2)
 }
 
-console.log(incrementItems([-1, -2, -3, -4]))
+console.log(stackBoxes(4))
