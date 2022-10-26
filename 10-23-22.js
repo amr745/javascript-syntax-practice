971d5359be8a03bdffff6c661e54680b11e54b4e
Ashley Richard <ashleymrichard@gmail.com>
2022-10-26 00:56:17 -0700

    Inches to Feet

--- 10-23-22.js
+++ 10-23-22.js
@@ -1,8 +1,24 @@
 //1. Leap Year Function
 
-function leapYear(year) {
-    for (i = 0; i <= year; i++) {
-        return i % 4 === 0 && i % 100 !== 0 || i % 400 === 0
-    }
+// function leapYear(year) {
+//     for (i = 0; i <= year; i++) {
+//         return i % 4 === 0 && i % 100 !== 0 || i % 400 === 0
+//     }
+// }
+// console.log(leapYear(2000))
+
+//2. Word without First Character
+
+// function newWord(str) {
+// 	return str.slice(1)
+// }
+
+// console.log(newWord("plum"))
+
+//3. Inches to Feet
+
+function inchesToFeet(inches) {
+	return Math.floor(inches / 12)
 }
-console.log(leapYear(2000))
+
+console.log(inchesToFeet(0.5))
