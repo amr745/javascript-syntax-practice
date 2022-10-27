bba2179308d096abd9969543d12c7c95764c03a2
Ashley Richard <ashleymrichard@gmail.com>
2022-10-27 06:34:48 -0700

    adds Array Indexing

--- 10-25-22.js
+++ 10-25-22.js
@@ -1,7 +1,15 @@
 //1. Find the Index (Part 1)
 
-function search(arr, item) {
-	return arr.indexOf(item)
+// function search(arr, item) {
+// 	return arr.indexOf(item)
+// }
+
+// console.log(search([1, 2, 3], 4))
+
+//2. Array Indexing
+
+function valueAt(arr, i) {
+	return arr[Math.floor(i)]
 }
 
-console.log(search([1, 2, 3], 4))
+console.log(valueAt([1, 2, 3, 4], 6.535355314 / 2))
