c3e9a40ab0a727c65cce80d6843cd89a9275d04b
Ashley Richard <ashleymrichard@gmail.com>
2022-11-01 01:07:51 -0700

    adds Moving House

--- 10-25-22.js
+++ 10-25-22.js
@@ -42,16 +42,26 @@
 // console.log(search([2, 4, 6, 8, 10], 8))
 // console.log(search([1, 3, 5, 7, 9], 11))
 
-//6. 
+//6. Buggy Code (Part 3)
 
-function sumArray(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i]
-    } 
-    return sumBuggy Code (Part 3)
+// function sumArray(arr) {
+//     let sum = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         sum += arr[i]
+//     } 
+//     return sum
+// }
+
+// console.log(sumArray([1, 2, 3, 4, 5]))
+// console.log(sumArray([-1, 0, 1]))
+// console.log(sumArray([0, 4, 8, 12]))
+
+//7. Moving House
+
+function yearsInOneHouse(age, moves) {
+	return Math.round(age / (moves + 1))
 }
 
-console.log(sumArray([1, 2, 3, 4, 5]))
-console.log(sumArray([-1, 0, 1]))
-console.log(sumArray([0, 4, 8, 12]))
+console.log(yearsInOneHouse(30, 1))
+console.log(yearsInOneHouse(15, 2))
+console.log(yearsInOneHouse(23, 2))
