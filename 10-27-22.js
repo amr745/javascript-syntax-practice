21bb53fdb2f7a913dc2698af00b036a855a7c448
Ashley Richard <ashleymrichard@gmail.com>
2022-11-04 20:01:19 -0700

    adds Burglary Series (19): Prevent Changes

--- 10-27-22.js
+++ 10-27-22.js
@@ -1,9 +1,25 @@
 //1. Among Us Imposter Formula
 
-function imposterFormula(i, p) {
-	return Math.round(100 * (i / p)) + "%"
-}
+// function imposterFormula(i, p) {
+// 	return Math.round(100 * (i / p)) + "%"
+// }
 
-console.log(imposterFormula(1, 10))
-console.log(imposterFormula(2, 5))
-console.log(imposterFormula(1, 8))
+// console.log(imposterFormula(1, 10))
+// console.log(imposterFormula(2, 5))
+// console.log(imposterFormula(1, 8))
+
+//2. Burglary Series (19): Prevent Changes
+
+function preventChanges(obj) {
+    // write your code here
+    Object.freeze(obj)
+      // don't use a return statement
+      
+      
+    // DON'T CHANGE OR REMOVE THE LINES BELOW
+    obj.noChanges = false;
+    obj.signature = "whatever";
+    return obj;
+  }
+
+  console.log(preventChanges({ noChanges: true }))
