7e633002ba80f52cfb841436a4ab935d697716e8
Ashley Richard <ashleymrichard@gmail.com>
2022-11-13 23:55:40 -0800

    adds Recreating the abs() Function

---
+++ 10-28-22.js
@@ -0,0 +1,12 @@
+// 1. Recreating the abs() Function
+
+function absolute(n) {
+	return n > 0 ? n : -n
+}
+
+console.log(absolute(-1.217197940))
+console.log(absolute(-12.1320))
+console.log(absolute(-544.0))
+console.log(absolute(4666))
+console.log(absolute(-3.14))
+console.log(absolute(250))
