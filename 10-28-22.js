dcb84903604747c4a15a953bf2b75f8b55b8f4dd
Ashley Richard <ashleymrichard@gmail.com>
2022-11-15 23:54:01 -0800

    adds Less Than, Greater Than

--- 10-28-22.js
+++ 10-28-22.js
@@ -1,12 +1,35 @@
 // 1. Recreating the abs() Function
 
-function absolute(n) {
-	return n > 0 ? n : -n
+// function absolute(n) {
+// 	return n > 0 ? n : -n
+// }
+
+// console.log(absolute(-1.217197940))
+// console.log(absolute(-12.1320))
+// console.log(absolute(-544.0))
+// console.log(absolute(4666))
+// console.log(absolute(-3.14))
+// console.log(absolute(250))
+
+//2. Less Than, Greater Than
+
+function arrBetween(num1, num2, arr) {
+    let stuff = []
+    for (let i = 0; i < arr.length; i++) {
+        return num2 > i < num1
+        // count = arr[i]
+        // consoele.log(Array.from(count))
+        // console.log(count)
+        // console.log(stuff.push(count))
+
+    }
+    // console.log(stuff.push(count))
+    // console.log(count)
+    console.log((count))
+    // console.log(stuff.push(count))
 }
 
-console.log(absolute(-1.217197940))
-console.log(absolute(-12.1320))
-console.log(absolute(-544.0))
-console.log(absolute(4666))
-console.log(absolute(-3.14))
-console.log(absolute(250))
+arrBetween(3, 8, [1, 5, 95, 0, 4, 7])
+// console.log(arrBetween(1, 10, [1, 10, 25, 8, 11, 6]))
+// console.log(arrBetween(7, 32, [1, 2, 3, 78]))
+// console.log()
