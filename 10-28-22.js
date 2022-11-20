ef8aad6b3571843e7cc79680de21d3662f508b06
Ashley Richard <ashleymrichard@gmail.com>
2022-11-19 23:13:44 -0800

    adds Four Passengers and a Driver

--- 10-28-22.js
+++ 10-28-22.js
@@ -13,23 +13,20 @@
 
 //2. Less Than, Greater Than
 
-function arrBetween(num1, num2, arr) {
-    let stuff = []
-    for (let i = 0; i < arr.length; i++) {
-        return num2 > i < num1
-        // count = arr[i]
-        // consoele.log(Array.from(count))
-        // console.log(count)
-        // console.log(stuff.push(count))
+// function arrBetween(num1, num2, arr) {
+// 	return arr.filter(ar => num1 < ar && ar < num2)
+// }
 
-    }
-    // console.log(stuff.push(count))
-    // console.log(count)
-    console.log((count))
-    // console.log(stuff.push(count))
-}
-
-arrBetween(3, 8, [1, 5, 95, 0, 4, 7])
+// console.log(arrBetween(3, 8, [1, 5, 95, 0, 4, 7]))
 // console.log(arrBetween(1, 10, [1, 10, 25, 8, 11, 6]))
 // console.log(arrBetween(7, 32, [1, 2, 3, 78]))
-// console.log()
+
+// 3. Four Passengers and a Driver
+
+function carsNeeded(n) {
+	return Math.ceil(n / 5)
+}
+
+console.log(carsNeeded(5))
+console.log(carsNeeded(11))
+console.log(carsNeeded(0))
