4937c97d10026a537aa613d0e62fb5e66b3a0ec9
Ashley Richard <ashleymrichard@gmail.com>
2022-11-23 22:08:35 -0800

    adds Minimal II: Boolean Redundancy

--- 11-22-22.js
+++ 11-22-22.js
@@ -1,10 +1,18 @@
-// Valid Zip Code
+// 1. Valid Zip Code
 
-function isValid(zip) {
-	return /^\d+$/.test(zip) && zip.length === 5
+// function isValid(zip) {
+// 	return /^\d+$/.test(zip) && zip.length === 5
+// }
+
+// console.log(isValid("59001"))
+// console.log(isValid("853a7"))
+// console.log(isValid("732 32"))
+// console.log(isValid("393939"))
+
+// 2. Minimal II: Boolean Redundancy
+
+function parity(n) {
+	return n % 2 === 0 ? "even" : "odd"
 }
 
-console.log(isValid("59001"))
-console.log(isValid("853a7"))
-console.log(isValid("732 32"))
-console.log(isValid("393939"))
+console.log(parity(16))
