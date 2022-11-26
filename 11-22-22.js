a7cf071bf3daa31455915960a5259c08ac9889d5
Ashley Richard <ashleymrichard@gmail.com>
2022-11-25 23:56:41 -0800

    changes

--- 11-22-22.js
+++ 11-22-22.js
@@ -30,18 +30,27 @@
 // 4. Convert All Array Items to String
 
 // function parseArray(arr) {
-//   let the = []
-// 	for (let i = 0; i < arr.length; i++) {
-//     the.push(arr[i].toString())
-//   }
-//   return the
+//   return arr.map(String)
 // }
 
-function parseArray(arr) {
-  return arr.map(String)
-}
+// console.log(parseArray([1, 2, "a", "b"]))
+// console.log(parseArray(["abc", 123, "def", 456]))
+// console.log(parseArray([1, 2, 3, 17, 24, 3, "a", "123b"]))
+// console.log(parseArray([]))
 
-console.log(parseArray([1, 2, "a", "b"]))
-console.log(parseArray(["abc", 123, "def", 456]))
-console.log(parseArray([1, 2, 3, 17, 24, 3, "a", "123b"]))
-console.log(parseArray([]))
+// 5. Repeating Letters
+
+// function doubleChar(str) {
+//   let arr = []
+// 	for (i=0; i < str.length; i++) {
+//     let two = str[i] + str[i]
+//     arr.push(two)
+//   }
+//   return  arr.toString().replaceAll(',', '')
+// }
+
+// console.log(doubleChar("String"))
+// console.log(doubleChar("Hello World!"))
+// console.log(doubleChar("1234!_ "))
+
+// 6.
