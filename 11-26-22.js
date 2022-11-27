62e4cffdbbf5b7591fd8709c9061fdfeea975a0d
Ashley Richard <ashleymrichard@gmail.com>
2022-11-26 22:23:15 -0800

    adds First Class, Second Class and Third Class Levers

--- 11-26-22.js
+++ 11-26-22.js
@@ -1,10 +1,20 @@
 // 1. Is the Last Character an "N"?
 
-function isLastCharacterN(word) {
-	return word.slice(-1) === "n" 
+// function isLastCharacterN(word) {
+// 	return word.endsWith('n')
+// }
+
+// console.log(isLastCharacterN("Aiden"))
+// console.log(isLastCharacterN("Piet"))
+// console.log(isLastCharacterN("Bert"))
+// console.log(isLastCharacterN("Dean"))
+
+// 2. First Class, Second Class and Third Class Levers
+
+function determineLever(arr) {
+	return arr[1] === "f" ? "first class lever" : arr[2] === "f" ? "second class lever" : "third class lever"
 }
 
-console.log(isLastCharacterN("Aiden"))
-console.log(isLastCharacterN("Piet"))
-console.log(isLastCharacterN("Bert"))
-console.log(isLastCharacterN("Dean"))
+console.log(determineLever(["e", "f", "l"]))
+console.log(determineLever(["e", "l", "f"]))
+console.log(determineLever(["f", "e", "l"]))
