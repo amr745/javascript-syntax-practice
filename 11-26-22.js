745b0263f3057f429bd5499e0af8cc9f0738f80e
Ashley Richard <ashleymrichard@gmail.com>
2022-11-26 22:50:12 -0800

    adds Luke, I Am Your ...

--- 11-26-22.js
+++ 11-26-22.js
@@ -11,10 +11,26 @@
 
 // 2. First Class, Second Class and Third Class Levers
 
-function determineLever(arr) {
-	return arr[1] === "f" ? "first class lever" : arr[2] === "f" ? "second class lever" : "third class lever"
+// function determineLever(arr) {
+// 	return arr[1] === "f" ? "first class lever" : arr[2] === "f" ? "second class lever" : "third class lever"
+// }
+
+// console.log(determineLever(["e", "f", "l"]))
+// console.log(determineLever(["e", "l", "f"]))
+// console.log(determineLever(["f", "e", "l"]))
+
+// 3. Luke, I Am Your ...
+
+function relationToLuke(name) {
+	let relationship = {
+        "Darth Vader" : "father",
+        "Leia" : "sister",
+        "Han" : "brother in law",
+        "R2D2" : "droid"
+        } 
+    return "Luke, I am your " + relationship[name] + "."
 }
 
-console.log(determineLever(["e", "f", "l"]))
-console.log(determineLever(["e", "l", "f"]))
-console.log(determineLever(["f", "e", "l"]))
+console.log(relationToLuke("Darth Vader"))
+console.log(relationToLuke("Leia"))
+console.log(relationToLuke("Han"))
