1702b9a8cc41ac472f039dcc0453fa963e628325
Ashley Richard <ashleymrichard@gmail.com>
2022-11-26 22:08:09 -0800

    adds Is the Last Character an N?

--- 11-26-22.js
+++ 11-26-22.js
@@ -1,1 +1,10 @@
 // 1. Is the Last Character an "N"?
+
+function isLastCharacterN(word) {
+	return word.slice(-1) === "n" 
+}
+
+console.log(isLastCharacterN("Aiden"))
+console.log(isLastCharacterN("Piet"))
+console.log(isLastCharacterN("Bert"))
+console.log(isLastCharacterN("Dean"))
